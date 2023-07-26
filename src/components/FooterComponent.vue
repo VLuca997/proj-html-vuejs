bfd334b8c531d9bbb9772ff9adeccb3c28f64499
VLuca997 <kulertoast997@gmail.com>
2023-07-26 03:53:00 +0200

    fine struttura ( mancano pochi dettagli)

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -7,14 +7,85 @@ export default {
 }
 </script>
 
-<template >
-    <div>
-        
+<template ><div class="background">
+
+
+    <div class="d-flex justify-content-around">
+        <div class="info-sites">
+            <div class="logo">
+                <img src="../assets/img/logo-footer-football.png" alt="">
+            </div>
+            <div class="icons d-flex">
+                <p>icons</p>
+                <p>info</p>
+            </div>
+            <div class="social-icon d-flex">
+                <p>faceb</p>
+                <p>inst</p>
+                <p>twit</p>
+            </div>
+        </div>
+        <div class="join-our ">
+            <h2>join brod</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur non aut dolorem vitae.</p>
+            <div class="subscribes ">
+                <div class="form-floating mb-3">
+                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                    <label for="floatingInput">Email address</label>
+                </div>
+            </div>
+        </div>
+        <div class="info-links d-flex">
+            <div class="education-info">
+                <ul>
+                    <li>
+                        EDUCATION
+                    </li>
+                    <li>
+                        ciao
+                    </li>
+                    <li>
+                        ciao
+                    </li>
+                    <li>
+                        ciao
+                    </li>
+                    <li>
+                        ciao
+                    </li>
+                </ul>
+            </div>
+            <div class="quick-links">
+                <ul>
+                    <li>
+                        QUICK LINKS
+                    </li>
+                    <li>
+                        ciao
+                    </li>
+                    <li>
+                        ciao
+                    </li>
+                    <li>
+                        ciao
+                    </li>
+                    <li>
+                        ciao
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
+    <div class="copyright text-center p-5">
+        @ 2022 Football club - All Rights reserved.
+    </div>
+</div>
 </template>
 
 <style lang="scss" scoped>
-
+        ul, li{
+            list-style: none;
+        }
     
 
 </style>
--- src/components/InfoComponent.vue
+++ src/components/InfoComponent.vue
@@ -27,7 +27,7 @@ export default {
 
         <div class="card justify-content-around" style="width: 18rem;">
             
-            <img src="../assets/img/news1-1-300x300.png" class="card-img-top img-fluid" alt="...">
+            <img src="../assets/img/news3-1-300x300.png" class="card-img-top img-fluid" alt="...">
             <p class="text-center">29.05.2022 - Football</p>
             <div class="card-body">
                 <h5 class="card-title fw-bold">What is Football?</h5>
@@ -41,11 +41,11 @@ export default {
 
         <div class="card justify-content-around" style="width: 18rem;">
             
-            <img src="../assets/img/news1-1-300x300.png" class="card-img-top img-fluid" alt="...">
+            <img src="../assets/img/news2-1-300x300.png" class="card-img-top img-fluid" alt="...">
             <p class="text-center">29.05.2022 - Football</p>
             <div class="card-body">
                 <h5 class="card-title fw-bold">What is Football?</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text">Some quick example text to build on the c content.</p>
                 <div class="d-flex justify-content-center">
                 
                     <a href="#" class="btn px-5 py-3 rounded-5 text-light bg-black"><p class="mb-0 fw-bold">More</p></a>
@@ -55,7 +55,7 @@ export default {
 
         <div class="card justify-content-around" style="width: 18rem;">
             
-            <img src="../assets/img/news1-1-300x300.png" class="card-img-top img-fluid" alt="...">
+            <img src="../assets/img/news4-1-300x300.png" class="card-img-top img-fluid" alt="...">
             <p class="text-center">29.05.2022 - Football</p>
             <div class="card-body">
                 <h5 class="card-title fw-bold">What is Football?</h5>
@@ -68,18 +68,53 @@ export default {
         </div>
     </div>
 
+    <div class="d-flex container-ads-pages w-100 py-3 justify-content-center">
+        <button class="page-1 "></button>
+        <button class="page-2 active"></button>
+        <button class="page-3"></button>
+        <button class="page-4"></button>
+    </div>
+
 </template>
 
 <style lang="scss" scoped>
 .card{
     width: 300px;
     margin: 5px;
+    height: fit-content;
+    img{
+        padding: 30px;
+        height: 300px;
+        background-position: center;
+        background-size: cover;
+    
+    }
 }
-img{
-    padding: 30px;
-    height: 300px;
-    background-position: center;
-    background-size: cover;
+.container-ads-pages{
+    height: 60px;
+    button{
+        margin: 0 2px;
+        height: 5px;
+    }
+    .page-1,.page-2,.page-3,.page-4{
+        padding: 0 15px;
+        border: none;
+        border: 1px solid gray;
+        border-radius: 20px;
+        &:hover{
+            background-color: lightcoral;
+        }
+        &.active{
+            background-color: cyan;
+            padding: 0 25px;
+            &:hover{
+                cursor: pointer;
+                background-color: blueviolet;
+
+            }
+        }
+    }
 
 }
+
 </style>
